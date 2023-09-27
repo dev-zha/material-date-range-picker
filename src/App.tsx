89da0975496ad1781b8c0f784f82959617a3ee32
zawhtetaung211 <zawhtetaung21199@gmail.com>
2023-09-28 00:28:27 +0630

    Add Description

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { Dayjs } from 'dayjs';
 import DateRangePicker from './DateRangePicker';
+import './App.css';
 
 export default function App() {
   const [value, setValue] = React.useState<(Dayjs | null)[]>([]);
@@ -13,6 +14,20 @@ export default function App() {
 
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <h1>Date Range Picker</h1>
+      <p>Build with Normal Material Date Picker</p>
+      <br />
+      <p>
+        {'Selected Date Range : '}
+        <span className="date-text">
+          {value[0]?.format('YYYY-MM-DD') || '-'}
+        </span>
+        {' to '}
+        <span className="date-text">
+          {value[1]?.format('YYYY-MM-DD') || '-'}
+        </span>
+      </p>
+      <br />
       <DateRangePicker value={value} onChange={handleChange} error="" />
     </LocalizationProvider>
   );
